e5274d7db4806e1dd90380199909cdc22b1f2dbc
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-05-06 18:46:22 +0200

    test app

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int		main(void)
+{
+    VideoCapture	stream("/dev/stdin");
+    Mat				edges;
+
+    if(!stream.isOpened())
+        return (-1);
+    namedWindow("edges",1);
+	while (42)
+    {
+        Mat frame;
+        stream >> frame;
+        cvtColor(frame, edges, CV_BGR2GRAY);
+        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        Canny(edges, edges, 0, 30, 3);
+        imshow("edges", edges);
+        if(waitKey(30) >= 0) break;
+    }
+    return 0;
+}
