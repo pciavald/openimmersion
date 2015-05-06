23ff1d8230015085fcfa0b5d7f763546b6327080
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-05-06 18:58:46 +0200

    scrip making and launching

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int		main(void)
 {
+	cout << "begin" << endl;
     VideoCapture	stream("/dev/stdin");
     Mat				edges;
 
+	cout << "stream open" << endl;
     if(!stream.isOpened())
         return (-1);
     namedWindow("edges",1);
 	while (42)
     {
+		cout << "frame" << endl;
         Mat frame;
         stream >> frame;
         cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
+        //GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        //Canny(edges, edges, 0, 30, 3);
         imshow("edges", edges);
         if(waitKey(30) >= 0) break;
     }
