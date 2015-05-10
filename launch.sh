4ed9181c815f4abfaffcd34017b45a3bff011c57
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-05-10 03:12:23 +0200

    bash

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 nc -l -p 5001 | ./build/getvideo &
-ssh 'pi@192.168.0.92' 'raspivid -t 0 -o - | nc 192.168.0.15 5001'
+ssh 'pi@192.168.0.80' 'raspivid -t 0 -o - | nc 192.168.0.15 5001'
