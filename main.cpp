8c3d60173b3d2f914e70aa2097162d8f1e75f1dd
Pierre-Alexis Ciavaldini <pierre-alexis@ciavaldini.fr>
2015-05-11 04:59:35 +0200

    test

--- main.cpp
+++ main.cpp
@@ -7,22 +7,22 @@ using namespace std;
 int		main(void)
 {
 	cout << "begin" << endl;
-    VideoCapture	stream("/dev/stdin");
-    Mat				edges;
+	VideoCapture	stream("/dev/stdin");
+	Mat				edges;
 
 	cout << "stream open" << endl;
-    if(!stream.isOpened())
-        return (-1);
-    namedWindow("edges",1);
+	if(!stream.isOpened())
+		return (-1);
+	namedWindow("edges",1);
 	while (42)
-    {
-        Mat frame;
-        stream >> frame;
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
-    }
-    return 0;
+	{
+		Mat frame;
+		stream >> frame;
+		cvtColor(frame, edges, CV_BGR2GRAY);
+		GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+		Canny(edges, edges, 0, 30, 3);
+		imshow("edges", edges);
+		if(waitKey(30) >= 0) break;
+	}
+	return 0;
 }
